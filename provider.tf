792dc91e236795f5a8b8264db3c99e1505ec3182
Minnie2629 <miniath100k@yahoo.fr>
2024-07-09 18:45:03 -0400

    lampstack

---
+++ provider.tf
@@ -0,0 +1,13 @@
+# configured aws provider with proper credentials
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
